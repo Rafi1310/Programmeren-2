24637b13e0b5e858002102ded12196d2af66e9e7
Rafi1310 <r.ijssel3@gmail.com>
2024-07-03 13:29:11 +0200

    aanpassing

--- index.js
+++ index.js
@@ -2,30 +2,47 @@ let button = document.querySelector('button');
 let foto = document.querySelector('#foto')
 let etenKnop = document.querySelector('#etenKnop')
 let drinkKnop = document.querySelector('#drinkKnop')
-const eetAudio = new Audio('eating.mp3')
-const drinkAudio = new Audio('drinking.mp3')
+const eetAudio = new Audio('audio/eating.mp3')
+const drinkAudio = new Audio('audio/drinking.mp3')
+const pikaAudio = new Audio('audio/pika.mp3')
+const timer = document.querySelector('#timer')
+let timeLeft = 30
 
 function changeFoto(){
-    if (foto.src.endsWith("pikachu_eet.jpg")) {
-        foto.src = "pikachu.jpg";
+    if (foto.src.endsWith("img/pikachu_eet.jpg")) {
+        foto.src = "img/pikachu.jpg";
     } 
     else {
-        foto.src = "pikachu.jpg";
+        foto.src = "img/pikachu.jpg";
     }
+    pikaAudio.play()
 }
 button.addEventListener('click', changeFoto)
 
 function eten(){
-    foto.src = 'pikachu_eet.jpg';
+    foto.src = 'img/pikachu_eet.jpg';
     console.log('eet')
     eetAudio.play()
 }
 
 function drinken(){
-    foto.src = 'pikachu_drink.jpg';
+    foto.src = 'img/pikachu_drink.jpg';
     console.log('drink')
     drinkAudio.play()
 }
 
 etenKnop.addEventListener('click', eten)
 drinkKnop.addEventListener('click', drinken)
+
+function updateTimer(){
+    if(timeLeft > 0){
+        timeLeft--
+        timer.textContent = timeLeft
+    }
+    else{
+        clearInterval(timerInterval)
+        foto.src = 'img/pikachu_dood.jpg'
+        alert('Pikachu is dood')
+    }
+}
+const timerInterval = setInterval(updateTimer, 1000)
