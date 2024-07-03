d75eb979f907a11f3368e959de4f162d23d5dd91
Rafi1310 <r.ijssel3@gmail.com>
2024-07-03 14:24:21 +0200

    Aanpassing

--- index.js
+++ index.js
@@ -2,9 +2,11 @@ let button = document.querySelector('button');
 let foto = document.querySelector('#foto')
 let etenKnop = document.querySelector('#etenKnop')
 let drinkKnop = document.querySelector('#drinkKnop')
+let slaapKnop = document.querySelector('#slaapKnop')
 const eetAudio = new Audio('audio/eating.mp3')
 const drinkAudio = new Audio('audio/drinking.mp3')
 const pikaAudio = new Audio('audio/pika.mp3')
+const sleepAudio = new Audio('audio/sleep.mp3')
 const timer = document.querySelector('#timer')
 const resetKnop = document.querySelector('#resetKnop')
 let timeLeft = 30
@@ -25,16 +27,35 @@ function eten(){
     foto.src = 'img/pikachu_eet.jpg';
     console.log('eet')
     eetAudio.play()
+    setTimeout(()=>{
+        foto.src = 'img/pikachu.jpg'
+    }, 2000)
+    resetTimer()
 }
 
 function drinken(){
     foto.src = 'img/pikachu_drink.jpg';
     console.log('drink')
     drinkAudio.play()
+    setTimeout(()=>{
+        foto.src = 'img/pikachu.jpg'
+    }, 2000)
+    resetTimer()
+}
+
+function slapen(){
+    foto.src = 'img/pikachu_sleep.avif';
+    console.log('sleep')
+    sleepAudio.play()
+    setTimeout(()=>{
+        foto.src = 'img/pikachu.jpg'
+    }, 5000)
+    resetTimer()
 }
 
 etenKnop.addEventListener('click', eten)
 drinkKnop.addEventListener('click', drinken)
+slaapKnop.addEventListener('click', slapen)
 
 function updateTimer(){
     if(timeLeft > 0){
